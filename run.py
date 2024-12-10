d5729544901e65d573e24ace9273c7c793ce29ca
mradovic38 <mihailo.radovic11@gmail.com>
2024-12-10 20:23:30 +0100

    Fix word recognition. Add two threshold types. Generate final prediction based on voting.

--- run.py
+++ run.py
@@ -1,30 +1,29 @@
 from dtw import recognize_word, find_similar_speaker
-from utils import load_speakers_from_folder, print_results
+from utils import print_results
 
 
 if __name__ == '__main__':
 
     DB_PATH = "sound_database"
     REF_NAME = '38-21'
-    speakers = load_speakers_from_folder(DB_PATH)
     
 
     print("Results on a word from a speaker who's sounds are in the dataset, but the said word is not in the dataset vocabulary:")
     INPUT_FILE = "test.wav"
-    recognized_word, top_word = recognize_word(INPUT_FILE, '', speakers)
+    recognized_word, top_word = recognize_word(INPUT_FILE, DB_PATH)
     print_results(recognized_word, top_word)
 
     print("\nResults on a word from a speaker who's sounds are not in the dataset, but the said word is in the dataset vocabulary:")
     INPUT_FILE = 'desno-test.wav'
-    recognized_word, top_word = recognize_word(INPUT_FILE, '', speakers)
+    recognized_word, top_word = recognize_word(INPUT_FILE, DB_PATH)
     print_results(recognized_word, top_word)
 
 
     print("\nResults on a word from a speaker who's sounds are in the dataset and the said word is in the dataset vocabulary:")
     INPUT_FILE = 'levo-test.wav'
-    recognized_word, top_word = recognize_word(INPUT_FILE, '', speakers)
+    recognized_word, top_word = recognize_word(INPUT_FILE, DB_PATH)
     print_results(recognized_word, top_word)
 
     print('\n Similar speaker to speaker named 38-21:')
 
-    find_similar_speaker(REF_NAME, speakers)
+    find_similar_speaker(REF_NAME, DB_PATH)
